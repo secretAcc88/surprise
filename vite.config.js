15b78ed843b873eb1e2f12f9ead92e37efff3cc4
secretAcc88 <shefoelgendy481@gmail.com>
2025-11-14 20:12:39 +0200

    add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'// https://vitejs.dev/config/
+import { VitePWA } from 'vite-plugin-pwa';
+
 export default defineConfig({
-  plugins: [react() ,tailwindcss()],
+  plugins: [react() ,tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Surprise App',          // Full app name
+        short_name: 'Surprise',        // Short name for home screen
+        start_url: '.',                // Start URL
+        display: 'standalone',         // Open without browser UI
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
 
